e2464ded6ef8c4ed98d4d4d8325b37ab3355f6eb
Digital-Grinnell <digital@grinnell.edu>
2019-12-01 22:28:21 -0600

    Revert "Added post 058 and restructured"
    This reverts commit 9569370548e566889a0fc60cceecf8146c897c43.

--- push-update.sh
+++ push-update.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-# cd ~/GitHub/blogs-McFateM
-current=`git symbolic-ref --short -q HEAD`
-git checkout ${current}
-perl -i.bak -lpe  'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
-# compile the site before copying to the new image
-hugo --ignoreCache --ignoreVendor --minify --debug --verbose
-echo "Hugo compilation is complete."
-echo "Starting docker image build..."
-docker image build -f push-update-Dockerfile --no-cache -t blog-update .
-echo "docker image build is complete."
+cd ~/Documents/GitHub/blogs-McFateM
+perl -i.bak -lpe 'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
+docker image build -t blog-update .
 docker login
 docker tag blog-update mcfatem/blogs-mcfatem:latest
 docker push mcfatem/blogs-mcfatem:latest
